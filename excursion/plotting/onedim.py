86c504a835273f76c2fc62af10f8cb6012486a3d
irinaespejo <iem244@nyu.edu>
2020-01-13 10:16:59 -0500

    1D example gpytorch

--- excursion/plotting/onedim.py
+++ excursion/plotting/onedim.py
@@ -1,5 +1,7 @@
 import numpy as np
 from utils import point_entropy
+from utils import point_entropy_gpytorch
+import torch
 
 def plot(axarr, gps, X, y_list, scandetails, batchsize = 1):
     gp_axes = axarr[:len(y_list)]
@@ -35,3 +37,49 @@ def plot(axarr, gps, X, y_list, scandetails, batchsize = 1):
     for x in X[:-1,0]:
         entraxis.axvline(x, c = 'grey', alpha = 0.2)
     entraxis.axvline(X[-1,0], c = 'r')
+
+
+def plot_gpytorch(axarr, gps, likelihood, X, y_list, scandetails, batchsize = 1):
+    gp_axes = axarr[:len(y_list)]
+    mu_stds = []
+
+    for ax in gp_axes:
+        if(ax != axarr[-1]):
+            ax.scatter(X,y_list)
+
+        for x in X[:-1]:
+            ax.axvline(x, c = 'grey', alpha = 0.2)
+        ax.axvline(X[-1], c = 'r')
+
+        if(ax != axarr[-1]):
+            gps.eval()
+            likelihood.eval()
+            observed_pred = likelihood(gps(torch.tensor(scandetails.plotX)))
+
+            # Get upper and lower confidence bounds
+            lower, upper = observed_pred.confidence_region()
+
+            # plot mean of the prediction function
+            ax.plot(scandetails.plotX[:,0], observed_pred.mean.detach().numpy()) #scandetails.plotX[:,0] is a numpy array
+            mu_stds.append(observed_pred)
+
+            # plot confidence region
+            ax.fill_between(scandetails.plotX[:,0], lower.detach().numpy(), upper.detach().numpy())
+
+            for func in scandetails.truth_functions:
+                ax.plot(scandetails.plotX.ravel(),func(scandetails.plotX).ravel(), c = 'k', linestyle = 'dashed')
+
+            for thr in scandetails.thresholds:
+                ax.hlines(thr, np.min(scandetails.plotX),np.max(scandetails.plotX), colors = 'grey')
+            ax.set_xlim(np.min(scandetails.plotX),np.max(scandetails.plotX))
+            ax.set_ylim(*scandetails.y_lim)
+
+    entraxis = axarr[-1]
+    entropies = point_entropy_gpytorch(mu_stds, scandetails.thresholds)
+    entraxis.fill_between(scandetails.plotX[:,0],-entropies,entropies, color='steelblue')
+    entraxis.set_ylim(-1,1)
+    entraxis.axhline(0, c = 'k')
+
+    for x in X[:-1]:
+        entraxis.axvline(x, c = 'grey', alpha = 0.2)
+    entraxis.axvline(X[-1], c = 'r')
--- excursion/utils.py
+++ excursion/utils.py
@@ -15,6 +15,24 @@ def point_entropy(mu_stds, thresholds):
         entropies.append(entropy)
     return np.mean(np.stack(entropies), axis=0)
 
+
+def point_entropy_gpytorch(mu_stds, thresholds):
+    thresholds = np.concatenate([[-np.inf],thresholds,[np.inf]])
+
+    entropies = []
+    for obs_pred in mu_stds:
+        entropy = 0
+        for j in range(len(thresholds) - 1):
+            p_within = norm(obs_pred.mean.detach().numpy(), obs_pred.stddev.detach().numpy()).cdf(thresholds[j+1]) - \
+                       norm(obs_pred.mean.detach().numpy(), obs_pred.stddev.detach().numpy()).cdf(thresholds[j])
+            p_within[p_within < 1e-9] = 1e-9
+            p_within[p_within > 1-1e-9] = 1-1e-9
+            entropy -= p_within * np.log(p_within)
+        entropies.append(entropy)
+    return np.mean(np.stack(entropies), axis=0)
+
+
+
 def mesh2points(grid,npoints_tuple):
     ndim = len(npoints_tuple)
     X = np.moveaxis(grid,0,ndim).reshape(int(np.product(npoints_tuple)),ndim)
