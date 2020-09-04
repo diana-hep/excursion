1efa0fcb0816ba43925df9c0163f82a301904904
irinaespejo <iem244@nyu.edu>
2020-09-04 08:25:38 -0400

    travis

--- testing/test_initialize_gp.py
+++ testing/test_initialize_gp.py
@@ -10,7 +10,7 @@ def test_init_gp():
 
     device = torch.device("cpu")
     n_init = 1
-    algorithmopts = yaml.safe_load(open("./algorithm_specs.yaml", "r"))
+    algorithmopts = yaml.safe_load(open("testing/algorithm_specs.yaml", "r"))
 
     # three toy examples
     for example in ["1Dtoyanalysis", "2Dtoyanalysis", "3Dtoyanalysis"]:
