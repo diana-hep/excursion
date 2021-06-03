9a0fa623eda5cecbef0ed2632c933c2393829181
irinaespejo <iem244@nyu.edu>
2021-06-03 17:23:51 +0200

    cleaning up

--- run-excursion.s
+++ run-excursion.s
@@ -8,42 +8,6 @@
 #SBATCH --cpus-per-task=1
 
 #go to directory
-cd /scratch/$USER/excursion/excursion
-
-#set environment
-module purge
-source /home/$USER/.bashrc
-
-module load anaconda3/4.3.1
-#module load cuda/9.0.176
-#module load pytorch/python3.6/0.3.0_4 
-conda activate excursion
-
-#module load cudnn/9.0v7.0.5
-
-#check envirnmt ok
-python -c "from __future__ import print_function; import torch; print(torch.version.cuda,torch.cuda.is_available())"
-python -c "from __future__ import print_function; import yaml"
-
-#deactivate
-#conda deactivate
-
-#end of script
-
-
-############################################################################
-
-
-#!/bin/bash
-#
-#SBATCH --job-name=excursionGPU_MES
-#SBATCH --nodes=1
-#SBATCH --time=00:10:00
-#SBATCH --mem=2GB
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=1
-
-#go to directory
 cd /scratch/$USER/excursion/
 
 #set environment
