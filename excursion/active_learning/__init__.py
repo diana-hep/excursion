ca842bec4d48d933930557b652240e4d204c8127
tutrie <40398781+tutrie@users.noreply.github.com>
2021-06-23 20:37:36 -0400

    move excursionEstimator class
    made it it's own module within excursion package

--- excursion/active_learning/__init__.py
+++
@@ -1,7 +0,0 @@
-# from excursion.active_learning import acquisition_functions as acq_func
-
-# acquisition_functions = {
-#    "PES": acq_func.PES,
-#    "MES": acq_func.MES,
-#    "MES_test": acq_func.MES_test
-# }
