4aaad7d8a9ee5182292db2c522d8bf181f46f697
tutrie <40398781+tutrie@users.noreply.github.com>
2021-06-29 20:57:40 -0400

    code to eyeball runtime

--- testing/testcases/fast_1D.py
+++ testing/testcases/fast_1D.py
@@ -34,16 +34,17 @@ def function_4(X: torch.Tensor) -> torch.Tensor:
     return f4
 
 
-true_functions = [function_2]
+true_functions = [function_1]
+
 
 # Define threshold list
-thresholds = torch.Tensor([2.0])
+thresholds = torch.Tensor([0.7])
 
 # Define grid for acquisition function
 n_dims = 1
 
 ## rangedef[i] = [lower_i, upper_i, n_i] for i in n_dims
-rangedef_1 = [0, 5, 400]
+rangedef_1 = [-1, 1, 100]
 rangedef = np.array([rangedef_1])
 
 grid_1 = torch.linspace(
@@ -58,3 +59,29 @@ X_plot = np.linspace(rangedef_1[0], rangedef_1[1], rangedef_1[2])
 
 mean_range = X  # default
 plot_y = torch.Tensor([-5, 30])
+
+
+
+#
+# # Define threshold list
+# thresholds = torch.Tensor([2.0])
+#
+# # Define grid for acquisition function
+# n_dims = 1
+#
+# ## rangedef[i] = [lower_i, upper_i, n_i] for i in n_dims
+# rangedef_1 = [0, 5, 400]
+# rangedef = np.array([rangedef_1])
+#
+# grid_1 = torch.linspace(
+#     start=rangedef_1[0], end=rangedef_1[1], steps=rangedef_1[2], dtype=torch.double
+# )
+# X = grid_1.view(-1, 1)
+#
+# ## Define grid for plotting, with same format as above, default same as X
+# plot_X = X
+#
+# X_plot = np.linspace(rangedef_1[0], rangedef_1[1], rangedef_1[2])
+#
+# mean_range = X  # default
+# plot_y = torch.Tensor([-5, 30])
