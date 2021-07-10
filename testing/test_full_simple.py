671aaa7c6b4603e7ebc3a263b0ac4940aa05ac7b
tutrie <40398781+tutrie@users.noreply.github.com>
2021-07-09 20:55:19 -0400

    changed value name to update

--- testing/test_full_simple.py
+++ testing/test_full_simple.py
@@ -4,28 +4,30 @@ import torch
 import yaml
 from excursion import init_gp, ExcursionSetEstimator
 from excursion.utils import load_example
+import unittest
 
+class TestFullSimple(unittest.TestCase):
 
-def test_full_simple():
+    def test_full_simple(self):
 
-    tol = 1e-6
-    device = torch.device("cpu")
-    ninit = 1
-    algorithmopts = yaml.safe_load(open("testing/algorithm_specs_full_test.yaml", "r"))
+        tol = 1e-6
+        device = torch.device("cpu")
+        ninit = 1
+        algorithmopts = yaml.safe_load(open("../testing/algorithm_specs_full_test.yaml", "r"))
+        print('hello\n')
+        # three toy examples
+        for example in ["1D_test"]:
+            testcase = load_example(example)
+            model, likelihood = init_gp(testcase, algorithmopts, ninit, device)
 
-    # three toy examples
-    for example in ["1D_test"]:
-        testcase = load_example(example)
-        model, likelihood = init_gp(testcase, algorithmopts, ninit, device)
-
-        estimator = ExcursionSetEstimator(
-            testcase, algorithmopts, model, likelihood, device
-        )
-
-        while estimator.this_iteration < algorithmopts["nupdates"]:
-            estimator.step(testcase, algorithmopts, model, likelihood)
-            model = estimator.update_posterior(
-                testcase, algorithmopts, model, likelihood
+            estimator = ExcursionSetEstimator(
+                testcase, algorithmopts, model, likelihood, device
             )
-    print(torch.abs(model.train_targets) <= tol)
-    assert type(torch.abs(model.train_targets) <= tol) != type(None)
+
+            while estimator.this_iteration < algorithmopts["nupdates"]:
+                estimator.step(testcase, algorithmopts, model, likelihood)
+                model = estimator.update_posterior(
+                    testcase, algorithmopts, model, likelihood
+                )
+        print(torch.abs(model.train_targets) <= tol)
+        self.assertTrue(type(torch.abs(model.train_targets) <= tol) != type(None))
